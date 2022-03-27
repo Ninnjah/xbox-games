56ae8010094349b4ff813e883d638b8aa3eb3685
Ninnjah <yand.man4@gmail.com>
2022-03-27 15:54:42 +0300

    Remove unnecessary handler

--- tgbot/handlers/admin.py
+++ tgbot/handlers/admin.py
@@ -11,8 +11,9 @@ async def admin_start(m: Message):
 
 
 def register_admin(dp: Dispatcher):
-    dp.register_message_handler(admin_start, commands=["start"], state="*", role=UserRole.ADMIN)
+    # dp.register_message_handler(admin_start, commands=["start"], state="*", role=UserRole.ADMIN)
     # # or you can pass multiple roles:
+    pass
     # dp.register_message_handler(admin_start, commands=["start"], state="*", role=[UserRole.ADMIN])
     # # or use another filter:
     # dp.register_message_handler(admin_start, commands=["start"], state="*", is_admin=True)
