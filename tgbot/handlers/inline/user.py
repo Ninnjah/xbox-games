f03a3f404e97895602c718ff62af090fa50a0b0e
Ninnjah <yand.man4@gmail.com>
2022-03-27 15:11:03 +0300

    Add torrent download progress keyboard

--- tgbot/handlers/inline/user.py
+++ tgbot/handlers/inline/user.py
@@ -1,5 +1,7 @@
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
+from tgbot.cb_data import torrent_status
+
 
 def download_kb(url: str) -> InlineKeyboardMarkup:
     keyboard = InlineKeyboardMarkup()
@@ -7,6 +9,19 @@ def download_kb(url: str) -> InlineKeyboardMarkup:
     keyboard.add(
         InlineKeyboardButton(
             text="Скачать игру", url=url
+        )
+    )
+
+    return keyboard
+
+
+def status_kb(hash: str) -> InlineKeyboardMarkup:
+    keyboard = InlineKeyboardMarkup()
+
+    keyboard.add(
+        InlineKeyboardButton(
+            text="Прогресс загрузки",
+            callback_data=torrent_status.new(hash)
         )
     )
 
